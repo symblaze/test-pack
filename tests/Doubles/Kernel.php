8da8bfe14a514478d4f0d38a4fd6e63e55c06cd0
imdhemy <imdhemy@gmail.com>
2023-12-19 15:05:08 +0100

    [feat] add kernel test case trait

---
+++ tests/Doubles/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symblaze\TestPack\Tests\Doubles;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+final class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
--- tests/TestCaseTest.php
+++ tests/TestCaseTest.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace Symblaze\TestPack\Tests;
 
+use Symblaze\TestPack\KernelTestTrait;
 use Symblaze\TestPack\TestCase;
 use Symblaze\TestPack\Tests\Doubles\FirstLevelTrait;
+use Symblaze\TestPack\Tests\Doubles\Kernel;
 use Symblaze\TestPack\Tests\Doubles\SecondLevelTrait;
 
 final class TestCaseTest extends TestCase
 {
     use FirstLeveLTrait;
     use SecondLeveLTrait;
+    use KernelTestTrait;
+
+
+    protected function setUp(): void
+    {
+        $_ENV['KERNEL_CLASS'] = Kernel::class;
+        
+        parent::setUp();
+    }
 
     /** @test */
     public function it_should_invoke_setup_template_methods_only_once(): void
@@ -27,5 +38,13 @@ final class TestCaseTest extends TestCase
 
         $this->assertTrue($this->invokedL1TearDown);
         $this->assertTrue($this->invokedL2TearDown);
+    }
+
+    /** @test */
+    public function it_can_create_a_kernel(): void
+    {
+        $kernel = self::createKernel();
+
+        $this->assertNotNull($kernel);
     }
 }
