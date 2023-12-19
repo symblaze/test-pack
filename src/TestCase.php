12245b3234cdbde156a425e6f47f248e2199deab
imdhemy <imdhemy@gmail.com>
2023-12-19 14:14:34 +0100

    [feat] add base test case
    - it invokes template methods

---
+++ src/TestCase.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symblaze\TestPack;
+
+use ReflectionClass;
+
+/**
+ * All test cases should extend this class
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->invokeTemplateMethods('setUp');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->invokeTemplateMethods('tearDown');
+
+        parent::tearDown();
+    }
+
+    private function invokeTemplateMethods(string $prefix): void
+    {
+        $traits = $this->usedTraits(static::class);
+
+        foreach ($traits as $trait) {
+            $reflection = new ReflectionClass($trait);
+            $methods = $reflection->getMethods();
+
+            foreach ($methods as $method) {
+                if ($method->name !== $prefix && str_starts_with($method->name, $prefix)) {
+                    $this->{$method->name}();
+                }
+            }
+        }
+    }
+
+    private function usedTraits(string $class): array
+    {
+        $traits = class_uses($class);
+
+        foreach ($traits as $trait) {
+            $traits += $this->usedTraits($trait);
+        }
+
+        return array_unique($traits);
+    }
+}
