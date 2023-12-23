1ccfdfa3eab7e54171ddd6164e638dbccca802ec
imdhemy <imdhemy@gmail.com>
2023-12-23 20:59:00 +0100

    feat(client): extract files from data array

--- src/Concern/InteractsWithClient.php
+++ src/Concern/InteractsWithClient.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symblaze\TestPack\Concern;
 
 use Symfony\Component\BrowserKit\AbstractBrowser;
+use Symfony\Component\HttpFoundation\File\UploadedFile as SymfonyUploadedFile;
 
 trait InteractsWithClient
 {
@@ -18,5 +19,26 @@ trait InteractsWithClient
     protected function client(): AbstractBrowser
     {
         return self::getClient();
+    }
+
+    protected function extractFilesFromDataArray(array &$data): array
+    {
+        $files = [];
+
+        foreach ($data as $key => $value) {
+            if ($value instanceof SymfonyUploadedFile) {
+                $files[$key] = $value;
+
+                unset($data[$key]);
+            }
+
+            if (is_array($value)) {
+                $files[$key] = $this->extractFilesFromDataArray($value);
+
+                $data[$key] = $value;
+            }
+        }
+
+        return $files;
     }
 }
