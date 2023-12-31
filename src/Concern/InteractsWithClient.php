8c9cb1d58666f142111ad2b5419bf8e24f17bbe2
Dhemy <imdhemy@gmail.com>
2024-01-01 00:39:23 +0100

    [Assert] assert response matches schema (#15)
    * feat(assert): assert response matches schema
    
    * feat(response): get response value by path

--- src/Concern/InteractsWithClient.php
+++ src/Concern/InteractsWithClient.php
@@ -6,6 +6,7 @@ namespace Symblaze\TestPack\Concern;
 
 use Symfony\Component\BrowserKit\AbstractBrowser;
 use Symfony\Component\HttpFoundation\File\UploadedFile as SymfonyUploadedFile;
+use Symfony\Component\HttpFoundation\Response;
 
 trait InteractsWithClient
 {
@@ -16,9 +17,9 @@ trait InteractsWithClient
         self::createClient($options, $server);
     }
 
-    protected function client(): AbstractBrowser
+    protected function client(AbstractBrowser $newClient = null): AbstractBrowser
     {
-        return self::getClient();
+        return self::getClient($newClient);
     }
 
     protected function actAs(object $user, string $firewallContext = 'main'): self
@@ -156,5 +157,59 @@ trait InteractsWithClient
         int $options = JSON_THROW_ON_ERROR
     ): void {
         $this->json('OPTIONS', $uri, $data, $headers, $options);
+    }
+
+    protected function response(): Response
+    {
+        return self::getResponse();
+    }
+
+    protected function getResponseDataByPath(string $path): mixed
+    {
+        $content = $this->response()->getContent();
+        $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+        $keys = explode('.', $path);
+
+        foreach ($keys as $key) {
+            if (is_array($data)) {
+                if ($this->isIndexKey($key)) {
+                    [$name, $index] = $this->parseIndexKey($key);
+                    if (! $this->isValidIndexKey($name, $index, $data)) {
+                        return null;
+                    }
+                    $data = $data[$name][$index];
+                } elseif ($this->isAssociativeKey($key, $data)) {
+                    $data = $data[$key];
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        return $data;
+    }
+
+    private function isIndexKey(string $key): bool
+    {
+        return 1 === preg_match('/^\w+\.\d+$/', $key);
+    }
+
+    private function isAssociativeKey(string $key, array $data): bool
+    {
+        return array_key_exists($key, $data);
+    }
+
+    private function isValidIndexKey(string $name, int $index, array $data): bool
+    {
+        return array_key_exists($name, $data) && is_array($data[$name]) && array_key_exists($index, $data[$name]);
+    }
+
+    private function parseIndexKey(string $key): array
+    {
+        [$name, $index] = explode('.', $key);
+
+        return [$name, (int)$index];
     }
 }
